71c870a49da631209d4371ee24e858252083d6b5
Vignunath008 <vignunathareddy@gmail.com>
2026-07-05 00:25:11 +0530

    Time: 62 ms (30.12%), Space: 29.6 MB (37.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,14 +4,14 @@ class Solution:
         n=len(height)
         i=0
         j=n-1
-        while i<n and j>0:
+        while i<j:
             c_w=j-i
             c_h=min(height[i],height[j])
             current_area=(c_w)*(c_h)
             max_water=max(current_area,max_water)
             if height[i]<height[j]:
                 i+=1
-            elif height[j]<=height[i]:
+            else:
                 j-=1
         return max_water
 
