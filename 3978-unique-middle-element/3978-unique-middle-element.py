e14495ef59284c5def80f84aa2ad10b4ef362c7e
Vignunath008 <vignunathareddy@gmail.com>
2026-07-05 00:43:12 +0530

    Time: 3 ms (27.42%), Space: 19.3 MB (57.48%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.py
+++ 3978-unique-middle-element/3978-unique-middle-element.py
@@ -2,13 +2,10 @@ class Solution:
     def isMiddleElementUnique(self, nums: list[int]) -> bool:
         n=len(nums)
         freq={}
-        l=0
-        h=n-1
-        mid=(l+h)//2
-        y=nums[mid]
+        mid=nums[n//2]
         for i in nums:
             freq[i]=freq.get(i,0)+1
-        if freq[y]==1:
+        if freq[mid]==1:
             return True
         else:
             return False
