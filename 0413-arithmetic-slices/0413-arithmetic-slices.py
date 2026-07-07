00536e96a2cf6453da72a8366fdec602f6f78066
Vignunath008 <vignunathareddy@gmail.com>
2026-07-07 23:30:47 +0530

    Time: 0 ms (100%), Space: 19.4 MB (49.79%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        if len(nums)<3:
+            return 0
+        curr=0
+        total=0
+        for i in range(2,len(nums)):
+            if nums[i]-nums[i-1]==nums[i-1]-nums[i-2]:
+                curr+=1
+                total+=curr
+            else:
+                curr=0
+        return total
+                
